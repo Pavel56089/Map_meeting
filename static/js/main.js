0bd34010d1682ab148de5b7222785fd0b33a81c9
Pavel Baharuev <pavel5609s@gmail.com>
2018-12-03 20:13:27 +0300

    Location set

--- static/js/main.js
+++ static/js/main.js
@@ -1,11 +1,18 @@
 ymaps.ready(init);
         function init() {
             var map = new ymaps.Map('map', {
-            center: [59.93772, 30.313622],
+            center: [55.75399399999374,37.62209300000001],
             zoom: 10,
             controls: ['geolocationControl', 'zoomControl']
         }),
-        firstButton = new ymaps.control.Button("Добавить себя на карту");
+        firstButton = new ymaps.control.Button({
+          data: {
+            content:'Добавить себя',
+          },
+          options: {
+            maxWidth: [30, 100, 150],
+          }
+        });
 
     map.controls.add(firstButton, {float: 'right'});
         }
